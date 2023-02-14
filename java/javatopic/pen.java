7015f1ebddf4757aa76c6349650c3e8a1a3d6c77
bhiruta <bjmistry94@gmail.com>
2023-02-14 22:23:40 +0000

    change

---
+++ java/javatopic/pen.java
@@ -0,0 +1,18 @@
+package javatopic;
+
+public class pen {
+    public static void main (String[] args) {
+        System.out.println("colour");
+        System.out.println("size");
+        System.out.println("price");
+/*
+I change package name to java to java topic
+I change class name from java to pen
+
+ */
+
+
+
+
+    }
+}
---
+++ java/manualtopic/topic.java
@@ -0,0 +1,21 @@
+package manualtopic;
+
+public class topic {
+    public static void main (String [] args) {
+        System.out.println("smoke testing");
+        System.out.println("saintary testing");
+        System.out.println("adhoc testing");
+        System.out.println("acceptance testing");
+        System.out.println("system testing");
+        System.out.println("integration testing");
+        System.out.println("confirmation testing");
+        System.out.println("unit testing");
+        System.out.println("load testing");
+        //I change the manual package to manual topic
+        // I change the class name from manual to topic
+
+
+
+
+    }
+}
